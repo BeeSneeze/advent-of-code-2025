3401518e698b37511e3a95d764bcd0ca74cc69f0
Hard working bee <beatrice.emma.soderqvist@gmail.com>
2025-12-07 09:59:27 +0100

    added a bit of a visualization

--- Problem7.cs
+++ Problem7.cs
@@ -1,8 +1,10 @@
 using Godot;
 using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 
-public partial class Problem7 : Node2D
+public partial class Problem7 : GridContainer
 {
 
     private int amountSplit = 0;
@@ -11,70 +13,95 @@ public partial class Problem7 : Node2D
     public override void _Ready()
     {
         var labRows = ParseData(LoadFromFile("res://problem_7.txt"));
-
-        //var experimentRows = labRows.Select(x => x.ToArray()).ToArray();
-
-        var startRow = labRows[0];
-
-        var countRow = new long[startRow.Length];
+        List<long[]> countMatrix = new List<long[]>();
+        var countRow = new long[labRows[0].Length]; // This counts the number of possible paths per square on the grid
 
         for(int k = 1; k < labRows.Length; k++)
         {
             labRows[k] = GetNewRow(labRows[k-1], labRows[k], countRow);
-            GD.Print(countRow);
+            countMatrix.Add((long[])countRow.Clone());
         }
-        
-        foreach(long num in countRow)
+
+        GD.Print("Total possible paths: " + countRow.Aggregate(0L, (x,y) => x+y).ToString());
+        GD.Print("Total amount of splits: " + amountSplit);
+
+        VisualizeAnswer(countMatrix);
+
+    }
+
+    private void VisualizeAnswer(List<long[]> countMatrix)
+    {
+        var scene = GD.Load<PackedScene>("res://RayGridSquare.tscn");
+
+        this.Columns = countMatrix.Count;
+        long highestNumber = 0;
+
+        foreach(var item in countMatrix)
         {
-            //GD.Print(num);
+            if(item.Max() > highestNumber)
+            {
+                highestNumber = item.Max();
+            }
         }
 
-        var totalPaths = countRow.Aggregate(0L, (x,y) => x+y);
+        foreach(var column in countMatrix)
+        {
+            foreach(var number in column)
+            {
+                var newSquare = scene.Instantiate<ColorRect>();
 
-        GD.Print(totalPaths);
+                if(number == 0)
+                {
+                    newSquare.Color = new Color(0.2f,0.2f,0.3f);
+                }
+                else if(number == highestNumber)
+                {
+                    newSquare.Color = new Color(0,1,0);
+                }
+                else
+                {
+                    newSquare.Color = new Color(0,1 + (float)Math.Log((0.001 + (float)number)/(float)highestNumber, 100000),0);
+                }
+
+                
+                AddChild(newSquare);
+                
+            }
+        }
+
     }
 
     private string GetNewRow(string oldRow, string newRow, long[] numberRow)
     {
-        var outRow = ((string)newRow.Clone()).ToArray();
-
-        long[] toBeAdded = new long[numberRow.Length];
+        var outRow = ((string)newRow.Clone()).ToArray(); // Need a char array to write to
+        long[] toBeAdded = new long[numberRow.Length]; // First calculate, then write
 
         for(int i = 0; i < oldRow.Length; i++)
         {
+            // Just propagate existing rays
+            if(oldRow[i] == '|' && newRow[i] == '.')
+                outRow[i] = '|';
+
+            // If it's the starting ray, provide one possible path
             if(oldRow[i] == 'S' && newRow[i] == '.')
             {
                 outRow[i] = '|';
                 numberRow[i] += 1;
             }
 
-            if(oldRow[i] == '|')
-            {
-                if(newRow[i] == '.')
-                {
-                    outRow[i] = '|';
-                }
-            }
-
+            // If there is a split, transfer number values to new paths
             if(oldRow[i] == '|' && newRow[i] == '^')
             {
-                toBeAdded[i] = -numberRow[i];
+                toBeAdded[i] = -numberRow[i]; // There are no rays beneath the splitter
                 amountSplit++;
                 if(i > 0)
                 {
-                    if(newRow[i-1] == '.')
-                    {
-                        outRow[i-1] = '|';
-                    }
+                    outRow[i-1] = '|';
                     toBeAdded[i-1] += numberRow[i];
-                    
                 }
                 if(i + 1 < oldRow.Length)
                 {
-                    if(newRow[i+1] == '.')
-                    {
-                        outRow[i+1] = '|';
-                    }
+                    outRow[i+1] = '|';
                     toBeAdded[i+1] += numberRow[i];
                 }
             }
@@ -85,14 +112,7 @@ public partial class Problem7 : Node2D
             numberRow[i] += toBeAdded[i];
         }
 
-
         return new string(outRow);
-    }
-
-
-    // Called every frame. 'delta' is the elapsed time since the previous frame.
-    public override void _Process(double delta)
-    {
     }
 
     private string[] ParseData(string unparsed)
