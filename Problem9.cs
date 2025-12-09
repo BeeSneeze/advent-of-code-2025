088d34309297e198e4cb00cd8cc2c438f0725fd5
Hard working bee <beatrice.emma.soderqvist@gmail.com>
2025-12-09 13:03:50 +0100

    some code cleanup

--- Problem9.cs
+++ Problem9.cs
@@ -1,7 +1,6 @@
 using Godot;
 using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 
 public partial class Problem9 : Control
 {
@@ -9,12 +8,10 @@ public partial class Problem9 : Control
     public override void _Ready()
     {
         var redList = InitProblem("res://problem_9.txt");
-
-        var redDotList = new List<RedDot>();
-
+        var dotList = new List<RedDot>();
         var turnAmount = 0;
 
-        redDotList.Add(new RedDot(redList[0].x, redList[0].y, FindOrientation(redList[redList.Count-1], redList[0]), FindOrientation(redList[0], redList[1])));
+        dotList.Add(new RedDot(redList[0].x, redList[0].y, FindOrientation(redList[redList.Count-1], redList[0]), FindOrientation(redList[0], redList[1])));
         for(int k = 1; k < redList.Count-1; k++)
         {
             var nextDot = new RedDot(redList[k].x, redList[k].y, FindOrientation(redList[k-1], redList[k]), FindOrientation(redList[k], redList[k+1]));
@@ -26,96 +23,69 @@ public partial class Problem9 : Control
             {
                 turnAmount -= 1;
             }
-            redDotList.Add(nextDot);
+            dotList.Add(nextDot);
         }
-        redDotList.Add(new RedDot(redList[redList.Count-1].x, redList[redList.Count-1].y, FindOrientation(redList[redList.Count-2], redList[redList.Count-1]), FindOrientation(redList[redList.Count-1], redList[0])));
-
-
-        foreach(var dot in redDotList)
-        {
-            GD.Print(dot);
-        }
+        dotList.Add(new RedDot(redList[redList.Count-1].x, redList[redList.Count-1].y, FindOrientation(redList[redList.Count-2], redList[redList.Count-1]), FindOrientation(redList[redList.Count-1], redList[0])));
 
         // If we do more clockwise turns, that means clockwise turns are inside oriented
         var systemIsClockwise = turnAmount > 0;
 
-        // Step 2: Figure out which ones are inside the area
-
-        var biggestRectangle = 0L;
-        
-        for(int a = 0; a < redDotList.Count; a++)
+        var biggestRectangle = 0L;        
+        for(int a = 0; a < dotList.Count; a++)
         {
-            for(int b = 0; b < redDotList.Count; b++)
+            for(int b = 0; b < dotList.Count; b++)
             {
-                var rectSize = (1 + Math.Abs(redDotList[a].X - redDotList[b].X)) * (1 + Math.Abs(redDotList[a].Y - redDotList[b].Y));
-                var aDotValid = ValidRectangle(redDotList[a], redDotList[b], systemIsClockwise);
-                var bDotValid = ValidRectangle(redDotList[b], redDotList[a], systemIsClockwise);
-                var overlapValid = NoOverlaps(redDotList[b], redDotList[a], redDotList, systemIsClockwise);
+                var rectSize = (1 + Math.Abs(dotList[a].X - dotList[b].X)) * (1 + Math.Abs(dotList[a].Y - dotList[b].Y));
+                var aDotValid = ValidRectangle(dotList[a], dotList[b], systemIsClockwise);
+                var bDotValid = ValidRectangle(dotList[b], dotList[a], systemIsClockwise);
+                var overlapValid = NoOverlaps(dotList[b], dotList[a], dotList, systemIsClockwise);
 
                 if(aDotValid && bDotValid && overlapValid)
                 {
-                    var validIntersections = NoIntersections(redDotList[b], redDotList[a], redDotList);
-                    if(!validIntersections)
-                    {
-                        //GD.Print("FOUND AN INTERSECTION!!");
-                    }
-                    if(rectSize > biggestRectangle && validIntersections )
+                    var validIntersections = NoIntersections(dotList[b], dotList[a], dotList);
+
+                    if(rectSize > biggestRectangle && validIntersections)
                     {
                         biggestRectangle = rectSize;
                     }
                 }
-
-                
             }
         }
 
         GD.Print(biggestRectangle);
     }
 
-    // Too Low:  93328320
-    // Too High: 4790063600
-    // Too High: 4583207265
-    // Wrong   : 4266799061
-    // ???     : 1516172795
-
-
-
     private bool IsBetween(long checkedValue, long sideA, long sideB)
     {
         if(sideA > sideB)
-        {
             return checkedValue < sideA && checkedValue > sideB;
-        }
-        else
-        {
-            return checkedValue > sideA && checkedValue < sideB;
-        }
+        
+        return checkedValue > sideA && checkedValue < sideB;
     }
 
     private bool NoIntersections(RedDot activeDot, RedDot comparisonDot, List<RedDot> dotList)
     {
-        for(int k = 0; k < dotList.Count-1; k++)
+        for(int k = 0; k < dotList.Count; k++)
         {
-            if(dotList[k].X - dotList[k+1].X == 0)
+            var current = dotList[k];
+            var next = dotList[(k+1) % (dotList.Count-1)];
+
+            if(current.X - next.X == 0)
             {
-                if(IsBetween(activeDot.Y, dotList[k].Y, dotList[k+1].Y) || IsBetween(comparisonDot.Y, dotList[k].Y, dotList[k+1].Y))
+                if(IsBetween(activeDot.Y, current.Y, next.Y) || IsBetween(comparisonDot.Y, current.Y, next.Y))
                 {
-                    if(IsBetween(dotList[k].X, activeDot.X, comparisonDot.X))
+                    if(IsBetween(current.X, activeDot.X, comparisonDot.X))
                     {
-                        //GD.Print(dotList[k], dotList[k+1]);
-                        //GD.Print(activeDot, comparisonDot);
                         return false;
                     }
                 }
             }
             else
             {
-                if(IsBetween(activeDot.X, dotList[k].X, dotList[k+1].X) || IsBetween(comparisonDot.X, dotList[k].X, dotList[k+1].X))
+                if(IsBetween(activeDot.X, current.X, next.X) || IsBetween(comparisonDot.X, current.X, next.X))
                 {
-                    if(IsBetween(dotList[k].Y, activeDot.Y, comparisonDot.Y))
+                    if(IsBetween(current.Y, activeDot.Y, comparisonDot.Y))
                     {
-                        //GD.Print(dotList[k], dotList[k+1]);
-                        //GD.Print(activeDot, comparisonDot);
                         return false;
                     }
                 }
@@ -127,9 +97,6 @@ public partial class Problem9 : Control
 
     private bool NoOverlaps(RedDot activeDot, RedDot comparisonDot, List<RedDot> dotList, bool systemClockwise)
     {
-        var above = activeDot.Y > comparisonDot.Y;
-        var toTheRight = activeDot.X > comparisonDot.X;
-
         foreach(var dot in dotList)
         {
             if(dot.ClockWise() != systemClockwise)
@@ -137,25 +104,16 @@ public partial class Problem9 : Control
                 continue;
             }
 
-
             if(dot.X == activeDot.X || dot.X == comparisonDot.X)
             {
-                if(above && dot.Y > comparisonDot.Y && dot.Y < activeDot.Y)
-                {
-                    return false;
-                }
-                else if(!above && dot.Y > activeDot.Y && dot.Y < comparisonDot.Y)
+                if(IsBetween(dot.Y, activeDot.Y, comparisonDot.Y))
                 {
                     return false;
                 }
             }
             if(dot.Y == activeDot.Y || dot.Y == comparisonDot.Y)
             {
-                if(toTheRight && dot.X > comparisonDot.X && dot.X < activeDot.X)
-                {
-                    return false;
-                }
-                else if(!toTheRight && dot.X > activeDot.X && dot.X < comparisonDot.X)
+                if(IsBetween(dot.X, activeDot.X, comparisonDot.X))
                 {
                     return false;
                 }
@@ -173,11 +131,7 @@ public partial class Problem9 : Control
             if(activeDot.X > comparisonDot.X)
             {
                 // Active is to the right
-                if(activeDot.InDir == Orientation.LEFT)
-                {
-                    return false;
-                }
-                if(activeDot.OutDir == Orientation.RIGHT)
+                if(activeDot.InDir == Orientation.LEFT || activeDot.OutDir == Orientation.RIGHT)
                 {
                     return false;
                 }
@@ -185,11 +139,7 @@ public partial class Problem9 : Control
             if(activeDot.X < comparisonDot.X)
             {
                 // Active is to the left
-                if(activeDot.InDir == Orientation.RIGHT)
-                {
-                    return false;
-                }
-                if(activeDot.OutDir == Orientation.LEFT)
+                if(activeDot.InDir == Orientation.RIGHT || activeDot.OutDir == Orientation.LEFT)
                 {
                     return false;
                 }
@@ -198,11 +148,7 @@ public partial class Problem9 : Control
             if(activeDot.Y > comparisonDot.Y)
             {
                 // Active is above
-                if(activeDot.InDir == Orientation.DOWN)
-                {
-                    return false;
-                }
-                if(activeDot.OutDir == Orientation.UP)
+                if(activeDot.InDir == Orientation.DOWN || activeDot.OutDir == Orientation.UP)
                 {
                     return false;
                 }
@@ -210,11 +156,7 @@ public partial class Problem9 : Control
             if(activeDot.Y < comparisonDot.Y)
             {
                 // Active is below
-                if(activeDot.InDir == Orientation.UP)
-                {
-                    return false;
-                }
-                if(activeDot.OutDir == Orientation.DOWN)
+                if(activeDot.InDir == Orientation.UP || activeDot.OutDir == Orientation.DOWN)
                 {
                     return false;
                 }
@@ -235,38 +177,27 @@ public partial class Problem9 : Control
         LEFT,
         RIGHT,
         UP,
-        DOWN,
-        IMMOBILE
+        DOWN
     }
 
     private Orientation FindOrientation((long x, long y) a, (long x, long y) b)
     {
 
-        if(a.x == b.x)
+        if(a.y < b.y && a.x == b.x)
         {
-            if(a.y == b.y)
-            {
-                return Orientation.IMMOBILE;
-            }
-            if(a.y < b.y)
-            {
-                return Orientation.UP;
-            }
-            else
-            {
-                return Orientation.DOWN;
-            }
+            return Orientation.UP;
+        }
+        else if(a.y > b.y && a.x == b.x)
+        {
+            return Orientation.DOWN;
+        }
+        else if(a.x < b.x)
+        {
+            return Orientation.RIGHT;
         }
         else
         {
-            if(a.x < b.x)
-            {
-                return Orientation.RIGHT;
-            }
-            else
-            {
-                return Orientation.LEFT;
-            }
+            return Orientation.LEFT;
         }
     }
 
@@ -275,23 +206,18 @@ public partial class Problem9 : Control
     private List<(long x, long y)> InitProblem(string path)
     {
         var data = ParseData(LoadFromFile(path));
-
         var redList = new List<(long x, long y)>();
-
         foreach(var row in data)
         {
             redList.Add((row.Split(',')[0].ToInt(), row.Split(',')[1].ToInt()));
         }
-
         return redList;
     }
 
-    private List<(int x, int y)> InitExample()
+    private List<(int x, int y)> InitFromExampleGraph()
     {
         var data = ParseData(LoadFromFile("res://problem_9_example.txt"));
-
         List<(int x, int y)> redList = new List<(int, int)>();
-
         for(int x = 0; x < data.Length; x++)
         {
             for(int y = 0; y < data[0].Length; y++)
@@ -302,14 +228,9 @@ public partial class Problem9 : Control
                 }
             }
         }
-
         return redList;
     }
 
-    // Called every frame. 'delta' is the elapsed time since the previous frame.
-    public override void _Process(double delta)
-    {
-    }
     private string[] ParseData(string unparsed)
     {
         var parsedData = unparsed.Split(System.Environment.NewLine);
